7accd89caa08ad6ebfc12b4598feb95e73938080
Sk. Waleur Rahman <waleur.jessore@gmail.com>
2019-09-09 04:12:51 +0600

    Update CSS for Section(3)

--- out/_next/static/runtime/webpack.js
+++ out/_next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "86d43defcf473e5ae0bd";
+/******/ 	var hotCurrentHash = "9a9e162d167ffb4489de";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
