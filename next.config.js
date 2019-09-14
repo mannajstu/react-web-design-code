e26b65666581ba18f1048fea106c6b79a01d7589
Sk. Waleur Rahman <waleur.jessore@gmail.com>
2019-09-15 03:22:07 +0600

    Git Page testing

---
+++ next.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    // some configuration
+    assetPrefix: process.env.NODE_ENV === 'production' ? '/{https://github.com/mannajstu/react-web-design-code}' : '',
+    // another configuration
+  }
